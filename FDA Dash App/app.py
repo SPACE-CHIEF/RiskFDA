029cd921b3f7d3a6907a15342ac6c463720a3dfc
averma1 <anika_verma@yahoo.com>
2019-07-15 10:30:20 -0400

    moved app.py into apps folder

--- FDA Dash App/app.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import flask
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server)
-app.config.suppress_callback_exceptions = True
