3781a9c4f72e34b1f2646b08f55e3389fcc68d32
averma1 <anika_verma@yahoo.com>
2019-07-19 11:32:15 -0400

    changed text and projection type in risk

--- FDA Dash App/apps/risk.py
+++ FDA Dash App/apps/risk.py
@@ -21,7 +21,7 @@ layout = [
     html.Div([
         html.Div([
             html.H1("Risk Score Classification")],
-            style={'textAlign': "center", "padding-bottom": "30"}),
+            style={'text-align': "center", "padding-bottom": "30"}),
         html.Div([
                   html.Div([
                       dcc.Slider(
@@ -43,12 +43,13 @@ layout = [
 def map_maker(year):
     dff = df[df['Year'] == year]
     print(dff.head(5))
+    dt = dff['Legal Name'] + '<br>' + 'Classification: ' + dff['Classification']
     trace = [
         go.Choropleth(
             locations=dff['Code'],
             locationmode='USA-states',
             z=dff['Score_Classification'],
-            text=dff['Legal Name'],
+            text=dt,
             colorscale="Portland",
             geo = 'geo2'
         )
@@ -56,7 +57,8 @@ def map_maker(year):
     layout = go.Layout(
         geo2 = dict(
             scope = 'usa',
-            projection=dict(type="albers usa"),
+            #projection=dict(type='natural earth'),
+            projection=go.layout.geo.Projection(type='albers usa')
         )
     )
     return {'data': trace, 'layout': layout}
